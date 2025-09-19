10ce8eae4dc3ffb6a4067a8dd8b933380e4e7170
Sajmul <sajmul1427@gmail.com>
2025-09-19 20:13:35 +0600

    nav menu active link

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,23 +1,39 @@
+"use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+type NavItem = {
+  path: string;
+  label: string;
+};
+
+export const navItems: NavItem[] = [
+  { path: "/", label: "Home" },
+  { path: "/products", label: "Products" },
+  { path: "/gallery", label: "Gallery" },
+  { path: "/about", label: "About" },
+  { path: "/contact", label: "Contact" },
+];
 
 const Navbar = () => {
+  const pathname = usePathname();
   return (
     <nav className="flex justify-between py-3 bg-gray-600 px-4">
       <h2 className="font-semibold">
         <Link href="/">NEXT JS</Link>
       </h2>
       <div className="space-x-4">
-        <Link className="font-medium text-lg" href="/">
-          Home
-        </Link>
-        <Link href="/products">Products</Link>
-        <Link href="/gallery">Gallery</Link>
-        <Link className="font-medium text-lg" href="/about">
-          About
-        </Link>
-        <Link className="font-medium text-lg" href="/contact">
-          Contact
-        </Link>
+        {navItems.map((item) => (
+          <Link
+            key={item.path}
+            className={`font-medium text-lg ${
+              pathname === item.path ? "text-green-300" : ""
+            }`}
+            href={item.path}
+          >
+            {item.label}
+          </Link>
+        ))}
       </div>
     </nav>
   );
